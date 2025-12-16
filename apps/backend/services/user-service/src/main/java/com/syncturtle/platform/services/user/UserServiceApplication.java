2116f254ba460adcdea517aff1e3860d670ba630
Sebastian <salsebastian13@gmail.com>
2025-12-16 13:20:40 -0600

    chore(backend): add Dockerfile and .env for local .env + .env.example for host machine runs and Dockerfile added for containerization of services. package.json added to follow yarn workspace + turbo convention. Not to be used by maven.

--- apps/backend/services/user-service/src/main/java/com/syncturtle/platform/services/user/UserServiceApplication.java
+++ apps/backend/services/user-service/src/main/java/com/syncturtle/platform/services/user/UserServiceApplication.java
@@ -2,7 +2,9 @@ package com.syncturtle.platform.services.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "com.syncturtle")
 public class UserServiceApplication {
   public static void main(String[] args) {
