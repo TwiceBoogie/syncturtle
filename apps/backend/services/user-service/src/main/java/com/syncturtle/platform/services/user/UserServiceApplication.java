926f30d7f6bf0b31923a3aecfa8b9f0f0d1837f0
Sebastian <salsebastian13@gmail.com>
2025-12-18 07:42:55 -0600

    refactor(user-service): unit/ shouldn't have @SpringBootTest

--- apps/backend/services/user-service/src/main/java/com/syncturtle/platform/services/user/UserServiceApplication.java
+++ apps/backend/services/user-service/src/main/java/com/syncturtle/platform/services/user/UserServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication(scanBasePackages = "com.syncturtle")
+@SpringBootApplication
 public class UserServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(UserServiceApplication.class, args);
