3ab3ae7ce19fcf05feeb8cec5649ba3901f58263
TwiceBoogie <81591971+TwiceBoogie@users.noreply.github.com>
2025-12-22 07:51:54 -0600

    Sync from enterprise (2c02aebc2e11c852dfe7a7c18ae9dbb3d03c8b84) (#2)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- apps/backend/platform/api-gateway/src/main/java/com/syncturtle/platform/infra/gateway/ApiGatewayApplication.java
+++ apps/backend/platform/api-gateway/src/main/java/com/syncturtle/platform/infra/gateway/ApiGatewayApplication.java
@@ -3,7 +3,7 @@ package com.syncturtle.platform.infra.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.syncturtle")
+@SpringBootApplication
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
